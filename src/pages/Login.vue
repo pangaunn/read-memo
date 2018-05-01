b721af52e37dc5a8f6ab13a17e849a0269337508
aunn <aunn_it08@hotmail.com>
2018-05-01 23:57:56 +0700

    add Login.vue

---
+++ src/pages/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="f-s-30px">Read Memooooo</div>
+      <button class="button" @click="login">Login</button>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  methods: {
+    login () {
+      console.log('login')
+    }
+  }
+}
+</script>
