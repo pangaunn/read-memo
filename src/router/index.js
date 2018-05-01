71256e5ea49fb23aa0ce236987de07eba9231056
aunn <aunn_it08@hotmail.com>
2018-05-02 00:04:51 +0700

    add pages/Home and update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import Login from '../pages/Login'
+import Home from '../pages/Home'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
 })
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './auth'
 Vue.use(Vuex)
 
 const debug = process.env.NODE_ENV !== 'production'
 export default new Vuex.Store({
-  modules: { },
+  modules: {
+    auth
+  },
   strict: debug
 })
